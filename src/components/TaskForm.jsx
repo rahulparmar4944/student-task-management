6fb067c61ecc1fb31c3fd06de8841446a01b870f
rahul parmar <rahulparmar4944@gmail.com>
2026-02-05 14:25:16 +0530

    [RP][TaskForm Add Task]

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+
+const TaskForm = ({ addTask }) => {
+  const [formData, setFormData] = useState({
+    title: "",
+    desc: "",
+    date: "",
+    priority: "Low",
+  });
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    const newErrors = {};
+
+    if (!formData.title.trim()) {
+      newErrors.title = "title is required.";
+    } else if (formData.title.length < 6) {
+      newErrors.title = "Minimum 6 character required.";
+    }
+
+    if (!formData.desc.trim()) {
+      newErrors.desc = "description is required.";
+    } else if (formData.desc.length <= 6) {
+      newErrors.desc = "Minimum 6 character required.";
+    }
+
+    if (!formData.date) {
+      newErrors.date = "date is required.";
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleInputChange = (e) => {
+    //console.log(e.target.name,e.target.value)
+
+    //e.target.name = e.target.value
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+
+    setErrors({
+      ...errors,
+      [e.target.name]: "",
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      addTask(formData);
+    }
+  };
+  const resetForm = () => {
+    setFormData({
+      title: "",
+      desc: "",
+      date: "",
+      priority: "Low",
+    });
+  };
+
+  return (
+    <>
+      <div className="add-task-card">
+        <h2 style={{ marginBottom: "15px" }}>Add New Task</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              name="title"
+              value={formData.title}
+              placeholder="Task Title"
+              onChange={handleInputChange}
+            />
+            {/* Error Massage */}
+            {errors.title && <span className="error-msg">{errors.title}</span>}
+          </div>
+
+          <div>
+            <textarea
+              placeholder="Description"
+              value={formData.desc}
+              name="desc"
+              rows="3"
+              onChange={handleInputChange}
+            />
+          </div>
+
+          <div style={{ display: "flex", gap: "10px" }}>
+            <div style={{ flex: 1 }}>
+              <input
+                type="date"
+                value={formData.date}
+                name="date"
+                onChange={handleInputChange}
+              />
+              {errors.date && <span className="error-msg">{errors.date}</span>}
+            </div>
+
+            <div style={{ flex: 1 }}>
+              <select
+                name="priority"
+                value={formData.priority}
+                onChange={handleInputChange}
+              >
+                <option value="Low">Low Priority</option>
+                <option value="Medium">Medium Priority</option>
+                <option value="High">High Priority</option>
+              </select>
+            </div>
+          </div>
+
+          <div
+            className="form-actions"
+            style={{ display: "flex", gap: "10px", marginTop: "10px" }}
+          >
+            <button type="submit" className="btn-primary" style={{ flex: 1 }}>
+              Add Task
+            </button>
+
+            <button type="button" className="btn-secondary" style={{ flex: 1 }}>
+              Clear
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default TaskForm;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,43 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar'
-import { Navigate, useNavigate } from 'react-router-dom'
-import TaskList from '../components/TaskList'
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import { Navigate, useNavigate } from "react-router-dom";
+import TaskList from "../components/TaskList";
+import TaskForm from "../components/TaskForm";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+  const [tasks, setTasks] = useState([]);
 
-  const navigate = useNavigate()
-  const [tasks, setTasks] = useState([])
-
-  const fetchData = async() =>{
+  const fetchData = async () => {
     try {
-    const response = await fetch("http://localhost:3000/tasks");
-    const data = await response.json();
-    setTasks(data);
-    }catch(error) {
-      console.log(error)
+      const response = await fetch("http://localhost:3000/tasks");
+      const data = await response.json();
+      setTasks(data);
+    } catch (error) {
+      console.log(error);
     }
   };
 
-    useEffect(()=>{
-      fetchData();
-    },[])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const handleLogout = () => {
-    console.log('click from dashboard')
-    localStorage.removeItem('loginData')
-    localStorage.removeItem('authData')
+    console.log("click from dashboard");
+    localStorage.removeItem("loginData");
+    localStorage.removeItem("authData");
     // localStorage.clear()
-    navigate('/login')
-  }
+    navigate("/login");
+  };
+
+  const handleAddTask = async (newTask) => {
+    const tasktoAdd = { ...newTask, comleted: false };
+    try {
+      const response = await fetch("http://localhost:3000/tasks", {
+        method: "POST",
+        headers: { "Content-Type": "applicaltion/json" },
+        body: JSON.stringify(tasktoAdd),
+      });
+      console.log(tasktoAdd);
+      const data = await response.json();
+      setTasks([...tasks, data]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
-    <Navbar title="Task Management" onLogout={handleLogout}/>
-    <h1> My Task </h1>
-    <TaskList tasks={tasks}/>
+      <Navbar title="Task Management" onLogout={handleLogout} />
+      <TaskForm addTask={handleAddTask} />
+      <h1> My Task </h1>
+      <TaskList tasks={tasks} />
     </div>
-  )
-}
+  );
+};
 
-
-export default Dashboard
+export default Dashboard;
